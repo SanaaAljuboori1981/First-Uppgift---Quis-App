190ccc63a3f15447f27992a1411e35273ed8bf50
Sanaa Aljuboori <sanaa.aljuboori@mediainstetutet.se>
2024-12-12 23:51:47 +0100

    Create syntax in main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
+
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}`;
+};
